2b2de72dfcb661382d2fcceb313e9793d57d6f47
Jon <goodnow.jonathan@gmail.com>
2014-02-08 11:46:52 -0500

    button sizing

--- wsgi/static/search/scripts.js
+++ wsgi/static/search/scripts.js
@@ -1,4 +1,21 @@
-function infoblockIn(){
+function infoblockIn()
+{
     var list = document.getElementsByClassName("infoblock");
-    for(var i = 0; i < list.length; i++){
-        list[i].style.opacity = 1;}}
+    for(var i = 0; i < list.length; i++)
+    {
+        list[i].style.opacity = 1;
+    }
+}
+
+function titleUp()
+{
+    var s = document.getElementById("search_form");
+    if(s.query.value != "")
+    {
+        var top_pad = document.getElementById("frontpageTitle");
+        var text_obj = document.getElementById("message");
+        top_pad.style.padding = "0px";
+        text_obj.style.opacity = "0";
+        s.submit();
+    }
+}
