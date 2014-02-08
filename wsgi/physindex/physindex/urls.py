86b95eb554cb1508d51ee5bff9f4d47718900505
Jon <goodnow.jonathan@gmail.com>
2014-02-07 22:15:19 -0500

    discovered admin

--- wsgi/physindex/physindex/urls.py
+++ wsgi/physindex/physindex/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+admin.autodiscover()
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
