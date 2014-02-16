2b2e6775837d0aeb1cbd8ae06f351debedb5ef46
Jon <goodnow.jonathan@gmail.com>
2014-02-16 11:40:04 -0500

    static config

--- wsgi/physindex/physindex/settings.py
+++ wsgi/physindex/physindex/settings.py
@@ -77,7 +77,10 @@ SOUTH_TESTS_MIGRATE = False
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/staticroot/'
+if 'OPENSHIFT_REPO_DIR' in os.environ:
+    STATIC_ROOT = os.path.join(os.environ.get('OPENSHIFT_REPO_DIR'), 'wsgi', 'static')
+else:
+    STATIC_ROOT = os.path.join(PROJECT_ROOT, STATIC_URL.strip("/"))
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
