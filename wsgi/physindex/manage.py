27c94d8e1e2f22d02ee713dc36a75ab278ea9163
Jon <goodnow.jonathan@gmail.com>
2014-04-07 21:52:21 -0400

    manage.py knows where we are

--- wsgi/physindex/manage.py
+++ wsgi/physindex/manage.py
@@ -7,9 +7,10 @@ if __name__ == "__main__":
 
     from django.core.management import execute_from_command_line
 
-    try:
+    if os.environ.has_key('OPENSHIFT_REPO_DIR'):
+        # we're on the server. use server settings
         execute_from_command_line(sys.argv)
-    except AttributeError:
+    else:
         # means we're running locally. use local_settings instead
         os.environ["DJANGO_SETTINGS_MODULE"] = "physindex.local_settings"
         execute_from_command_line(sys.argv)
