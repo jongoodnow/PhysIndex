d5db2ef5cac886b186631e8e8ec0409687050b7a
Jon <goodnow.jonathan@gmail.com>
2014-05-03 18:12:07 -0400

    relocated general settings to base_settings

--- physindex/physindex/settings.py
+++ physindex/physindex/settings.py
@@ -9,8 +9,7 @@ from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS as TCP
 from unipath import FSPath as Path
 import urlparse
 import imp
-
-BASE = Path(__file__).absolute().ancestor(2)
+from base_settings import *
 
 ON_OPENSHIFT = False
 if os.environ.has_key('OPENSHIFT_REPO_DIR'):
@@ -38,56 +37,20 @@ DATABASES = {
     }
 }
 
-# Hosts/domain names that are valid for this site; required if DEBUG is False
-# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ['.physindex.com',
                  '.physindex.com.',
                  'production-physindex.rhcloud.com',
                  'production-physindex.rhcloud.com.']
-
-TIME_ZONE = 'America/New_York'
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
-
-SITE_ID = 1
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
-USE_I18N = True
-
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale.
-USE_L10N = True
-
-# If you set this to False, Django will not use timezone-aware datetimes.
-USE_TZ = True
 
 if 'OPENSHIFT_DATA_DIR' in os.environ:
     MEDIA_ROOT = os.path.join(os.environ.get('OPENSHIFT_DATA_DIR'), 'media')
 else:
     MEDIA_ROOT = os.path.join(BASE, *MEDIA_URL.strip("/").split("/"))
 
-MEDIA_URL = '/m/'
-
-# South Settings
-SOUTH_TESTS_MIGRATE = False
-
 if 'OPENSHIFT_REPO_DIR' in os.environ:
     STATIC_ROOT = os.path.join(os.environ.get('OPENSHIFT_REPO_DIR'), 'wsgi', 'static')
 else:
     STATIC_ROOT = os.path.join(BASE, STATIC_URL.strip("/"))
-
-STATIC_URL = '/static/'
-
-# List of finder classes that know how to find static files in
-# various locations.
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-)
 
 # Make a dictionary of default keys
 default_keys = { 'SECRET_KEY': 'vm4rl5*ymb@2&d_(gc$gb-^twq9w(idasofjiopewq39$43' }
@@ -101,13 +64,6 @@ if ON_OPENSHIFT:
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = use_keys['SECRET_KEY']
 
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
-)
-
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -116,33 +72,6 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
-
-ROOT_URLCONF = 'physindex.urls'
-
-# Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'physindex.wsgi.application'
-
-TEMPLATE_DIRS = (
-    BASE.child('templates'),
-)
-
-TEMPLATE_CONTEXT_PROCESSORS = TCP + (
-    'django.core.context_processors.request',
-)
-
-SUIT_CONFIG = {
-    # header
-    'ADMIN_NAME': 'Physindex Admin',
-    'HEADER_DATE_FORMAT': 'l, F j, Y',
-    'HEADER_TIME_FORMAT': 'H:i',
-
-    # menu
-    'SEARCH_URL': '/admin/auth/user/',
-    'MENU_ICONS': {
-       'sites': 'icon-leaf',
-       'auth': 'icon-lock',
-    },
-}
 
 INSTALLED_APPS = (
     'django.contrib.auth',
