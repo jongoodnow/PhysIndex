bb7ee670ab81940bad7a70834ab990cd6f9d1835
Jon <goodnow.jonathan@gmail.com>
2015-01-04 18:40:28 -0500

    prevented sql keywords from being submittable

--- wsgi/static/search/js/scripts.js
+++ wsgi/static/search/js/scripts.js
@@ -10,12 +10,23 @@ function infoblockIn()
     1);
 }
 
+function ContainsAny(str, items){
+    for(var i in items){
+        var item = items[i];
+        if (str.indexOf(item) > -1){
+            return true;
+        }
+    }
+    return false;
+}
+
 function titleUp()
 {
     setTimeout(function(){
         var s = document.getElementById("search_form");
-        if(s.query.value != "")
-        {
+        if(s.query.value != "" && !ContainsAny(s.query.value.toLowerCase(), 
+            ['select', 'union', 'benchmark', 'md5', 'db_name', 'concat', 'null', 'drop']
+        )){
             var top_pad = document.getElementById("frontpageTitle");
             var text_obj = document.getElementById("message");
             var loading = document.getElementById("load");
@@ -26,4 +37,13 @@ function titleUp()
         }
     },
     1);
+}
+
+function checkForm(){
+    var s = document.getElementById("search_form");
+    if(s.query.value != "" && !ContainsAny(s.query.value.toLowerCase(), 
+        ['select', 'union', 'benchmark', 'md5', 'db_name', 'concat', 'null', 'drop']
+    )){
+        s.submit();
+    }
 }
