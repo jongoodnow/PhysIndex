4e97e475c3327aed9d53e5d8f25f7f72c6c71f8e
Jon Goodnow <goodnow.jonathan@gmail.com>
2020-04-23 16:16:25 -0400

    Delete old website

--- .openshift/cron/weekly/backup_postgres.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Backs up the OpenShift PostgreSQL database for this application
-# by Skye Book <skye.book@gmail.com>
- 
-NOW="$(date +"%Y-%m-%d")"
-FILENAME="$OPENSHIFT_DATA_DIR/$OPENSHIFT_APP_NAME.$NOW.backup.sql.gz"
-pg_dump $OPENSHIFT_APP_NAME | gzip > $FILENAME
